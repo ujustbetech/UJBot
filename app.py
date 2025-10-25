e7a3ea3267a7ebb4aaeea8fdf361b570f9c6febb
NishikaMehta <nishikam3516@gmail.com>
2025-10-25 11:31:16 +0530

    Initial commit

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, jsonify, session
+import os
+
 
 app = Flask(__name__)
 app.secret_key = "supersecret"
@@ -256,4 +258,5 @@ def get_response():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
